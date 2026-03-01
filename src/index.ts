c9331daf5dc7efd99026fd8a484abc1064ce35be
Caleb-Script <caleb-script@outlook.de>
2026-03-01 16:46:13 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export * from "./enums/user-type.enum.js";
+export * from "./enums/contact-options-type.enum.js";
+export * from "./enums/gender-type.enum.js";
+export * from "./enums/interest-type.enum.js";
+export * from "./enums/marital-status-type.enum.js";
+export * from "./enums/person-status.enum.js";
+export * from "./enums/relationship-type.enum.js";
+export * from "./enums/phone-number-type.enum.js";
+export * from "./enums/role-type.enum.js";
+export * from "./enums/status-type.enum.js";
+
+
+export * from "./dtos/mail.dto.js";
